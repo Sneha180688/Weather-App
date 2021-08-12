6f3e8af0a7666d8c40c5350afcd130ada5b55d1d
Sneha180688 <88253575+Sneha180688@users.noreply.github.com>
2021-08-12 17:26:33 -0600

    Added forecast in JS

--- Source/Source.js
+++ Source/Source.js
@@ -74,3 +74,33 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+function dispalyForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
+
+  let forecastHTML = `<div class = "row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+<div class = "weather-forecast-date">${day}</div>
+  <img
+        src="https://ssl.gstatic.com/onebox/weather/48/rain_s_cloudy.png" alt="rain"
+            />
+            <div class = "weather-forecast-temperatures">
+            <span class = "weather-forecast-temperature-max"> 18° </span>
+            <span class = "weather-forecast-temperature-min"> 12° </span>
+            </div>
+            </div>
+`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
+dispalyForecast();
