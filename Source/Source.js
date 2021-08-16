6080f086cc28f88d4874d1d82146fa2cd5f7300d
Sneha180688 <88253575+Sneha180688@users.noreply.github.com>
2021-08-15 21:59:40 -0600

    Removed the forecast

--- Source/Source.js
+++ Source/Source.js
@@ -85,51 +85,11 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-function formatDate(timestamp) {
+function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
   return days[day];
 }
 
-function dispalyForecast(response) {
-  console.log(response.data.daily);
-
-  let forecastElement = document.querySelector("#forecast");
-
-  let forecastHTML = `<div class = "row">`;
-  forecastHTML.forEach(function (forecastDay, index) {
-    if (index < 6) {
-      forecast =
-        forecastHTML +
-        `
-      <div class="col-2">
-<div class = "weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-  <img
-        src="https://openweather.org/img/wn/${
-          forecastDay.weather[0].icon
-        }@2x.png" alt=""
-            />
-            <div class = "weather-forecast-temperatures">
-            <span class = "weather-forecast-temperature-max"> ${Math.round(
-              forecastDay.temp.max
-            )}° </span>
-            <span class = "weather-forecast-temperature-min"> ${Math.round(
-              forecastDay.temp.min
-            )}° </span>
-            </div>
-            </div>
-`;
-    }
-  });
-
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
-}
-
-function getForecast(coordinates) {
-  console.log(coordinates);
-  let apiKey = "5b582e413e399c28874a89c57e26dd21";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(dispalyForecast);
-}
+search("Albuquerque");
